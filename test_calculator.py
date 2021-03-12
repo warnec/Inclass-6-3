64450e151acdb23da7d0abd665de63991d96cb47
warnec <warnec@oregonstate.edu>
2021-03-11 16:03:20 -0800

    at last, everything should be working now

--- test_calculator.py
+++ test_calculator.py
@@ -8,10 +8,10 @@ import calculator
 class TestCalculator:
 
     def test_add(self):
-        assert 5 == calculator.add(1, 4)
+        assert 5 == calculator.addition(1, 4)
 
     def test_subtract(self):
-        assert 2 == calculator.subtract(5, 3)
+        assert 2 == calculator.subtraction(5, 3)
 
     def test_multiply(self):
         assert 2 == calculator.multiply(1, 2)
